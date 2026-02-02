27a3c1b592d3f5b760d9d7b24f58198c1a11fdb3
ghaithkhalfaoui <ghaithkhalfaoui982@gmail.com>
2026-02-02 20:59:40 +0100

    Interface Gestion Publication

---
+++ GestionPublication/main.cpp
@@ -0,0 +1,11 @@
+#include "gestionpublication.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    GestionPublication w;
+    w.show();
+    return a.exec();
+}
