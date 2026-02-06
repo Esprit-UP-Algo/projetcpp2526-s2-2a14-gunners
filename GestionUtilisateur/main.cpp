03ab7e143018d153ba102e6cc7cc45e92e5a312d
dalianiba17k <your-email@example.com>
2026-02-06 21:10:36 +0100

    couleur interface

---
+++ GestionUtilisateur/main.cpp
@@ -0,0 +1,32 @@
+#include "gestionpublication.h"
+
+#include <QApplication>
+#include <QStyleFactory>
+#include <QPalette>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    // 🔵 Forcer le style clair
+    a.setStyle(QStyleFactory::create("Fusion"));
+
+    // 🔵 Palette blanche
+    QPalette palette;
+    palette.setColor(QPalette::Window, Qt::white);
+    palette.setColor(QPalette::WindowText, Qt::black);
+    palette.setColor(QPalette::Base, Qt::white);
+    palette.setColor(QPalette::AlternateBase, QColor(245, 245, 245));
+    palette.setColor(QPalette::Text, Qt::black);
+    palette.setColor(QPalette::Button, QColor(240, 240, 240));
+    palette.setColor(QPalette::ButtonText, Qt::black);
+    palette.setColor(QPalette::Highlight, QColor(0, 120, 215));
+    palette.setColor(QPalette::HighlightedText, Qt::white);
+
+    a.setPalette(palette);
+
+    GestionPublication w;
+    w.show();
+
+    return a.exec();
+}
