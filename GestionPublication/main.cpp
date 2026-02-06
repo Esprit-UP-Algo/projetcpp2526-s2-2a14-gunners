03c6685fed06bbb2eb98b3a46bb072fe4131c9bc
ghaithkhalfaoui <ghaithkhalfaoui982@gmail.com>
2026-02-06 18:37:03 +0100

    module Publication

--- GestionPublication/main.cpp
+++ GestionPublication/main.cpp
@@ -2,10 +2,10 @@
 
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     GestionPublication w;
     w.show();
     return a.exec();
 }
+
